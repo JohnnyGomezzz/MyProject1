3425a9c95426bfbc3092365d46d8b141b0d1e112
JohnnyGomezzz <seldesign@mail.ru>
2022-05-02 18:48:00 +0300

    Lesson 5 main smth added

--- core/src/ru/johnnygomezzz/myproject/screens/MainMenu.java
+++ core/src/ru/johnnygomezzz/myproject/screens/MainMenu.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -15,18 +16,57 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenu implements Screen, InputProcessor {
-    final Game game;
-    SpriteBatch batch;
-    Sprite gameTitle;
-    Sprite buttonPlay;
-    Sprite buttonExit;
-    TextureAtlas mainAtlas;
+    private final Game game;
+    private SpriteBatch batch;
+    private Sprite gameTitle;
+    private Sprite buttonPlay;
+    private Sprite buttonPlayHighlighted;
+    private Sprite buttonExit;
+    private Sprite buttonExitHighlighted;
+    private TextureAtlas mainAtlas;
+
+    private boolean play;
+    private boolean exit;
+
+    private Vector2 cursorVec;
+    private Rectangle rectanglePlayH;
+    private Rectangle rectanglePlay;
+    private Rectangle rectangleExitH;
+    private Rectangle rectangleExit;
+
+    private OrthographicCamera camera;
 
     public MainMenu(Game game) {
         this.game = game;
         Gdx.input.setInputProcessor(this);
 
         batch = new SpriteBatch();
+
+        mainAtlas = new TextureAtlas("atlas/main.atlas");
+
+        gameTitle = new Sprite(mainAtlas.findRegion("title"));
+        gameTitle.setPosition(Gdx.graphics.getWidth() / 2f - gameTitle.getWidth() / 2f, Gdx.graphics.getHeight() - gameTitle.getHeight() - 50);
+
+        buttonPlay = new Sprite(mainAtlas.findRegion("buttons-play"));
+        buttonPlay.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f);
+        buttonPlayHighlighted = new Sprite(mainAtlas.findRegion("buttons-play-light"));
+        buttonPlayHighlighted.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f);
+
+        buttonExit = new Sprite(mainAtlas.findRegion("buttons-exit"));
+        buttonExit.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f - 100);
+        buttonExitHighlighted = new Sprite(mainAtlas.findRegion("buttons-exit-light"));
+        buttonExitHighlighted.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f - 100);
+
+        cursorVec = getPosition();
+        rectanglePlayH = buttonPlayHighlighted.getBoundingRectangle();
+        rectangleExitH = buttonExitHighlighted.getBoundingRectangle();
+        rectanglePlay = buttonPlay.getBoundingRectangle();
+        rectangleExit = buttonExit.getBoundingRectangle();
+
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.x = Gdx.graphics.getWidth() / 2f;
+        camera.position.y = Gdx.graphics.getHeight() / 2f;
+        camera.update();
     }
 
     @Override
@@ -38,27 +78,31 @@ public class MainMenu implements Screen, InputProcessor {
     public void render(float delta) {
         ScreenUtils.clear(Color.LIGHT_GRAY);
 
-        mainAtlas = new TextureAtlas("atlas/main.atlas");
-
-        gameTitle = new Sprite(mainAtlas.findRegion("title"));
-        gameTitle.setPosition(Gdx.graphics.getWidth() / 2f - gameTitle.getWidth() / 2f, Gdx.graphics.getHeight() - gameTitle.getHeight() - 50);
-
-        buttonPlay = new Sprite(mainAtlas.findRegion("buttons-play"));
-        buttonPlay.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f);
-
-        buttonExit = new Sprite(mainAtlas.findRegion("buttons-exit"));
-        buttonExit.setPosition(Gdx.graphics.getWidth() / 3f, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 2f - 100);
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
         gameTitle.draw(batch);
-        buttonPlay.draw(batch);
-        buttonExit.draw(batch);
+        if (play) {
+            buttonPlayHighlighted.draw(batch);
+        } else {
+            buttonPlay.draw(batch);
+        }
+        if (exit) {
+            buttonExitHighlighted.draw(batch);
+        } else {
+            buttonExit.draw(batch);
+        }
         batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        Gdx.graphics.setWindowedMode(width, height);
+        camera.viewportHeight = height;
+        camera.viewportWidth = width;
+        camera.position.x = Gdx.graphics.getWidth() / 2f;
+        camera.position.y = Gdx.graphics.getHeight() / 2f;
+        camera.update();
     }
 
     @Override
@@ -78,7 +122,8 @@ public class MainMenu implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        mainAtlas.dispose();
     }
 
     @Override
@@ -103,10 +148,6 @@ public class MainMenu implements Screen, InputProcessor {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        Vector2 cursorVec = getPosition();
-        Rectangle rectanglePlay = buttonPlay.getBoundingRectangle();
-        Rectangle rectangleExit = buttonExit.getBoundingRectangle();
-
         if (rectanglePlay.contains(cursorVec)) {
             dispose();
             game.setScreen(new GameProcess(game));
@@ -116,7 +157,7 @@ public class MainMenu implements Screen, InputProcessor {
             dispose();
             Gdx.app.exit();
         }
-        return true;
+        return false;
     }
 
     @Override
@@ -126,7 +167,16 @@ public class MainMenu implements Screen, InputProcessor {
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        return false;
+        play = false;
+        exit = false;
+
+        if (rectanglePlay.contains(cursorVec)) {
+            play = true;
+        }
+        if (rectangleExit.contains(cursorVec)) {
+            exit = true;
+        }
+        return true;
     }
 
     @Override
